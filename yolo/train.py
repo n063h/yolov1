207f12302c5fefe12caf02a1c50861761509fdd9
hang <20210240109@fudan.edu.cn>
2021-01-22 01:44:44 +0800

    bug fix

--- yolo/train.py
+++ yolo/train.py
@@ -56,6 +56,7 @@ def t(load_path=None,fronzen=True,offset=0):
     torch.autograd.set_detect_anomaly(True)
 
     best_eval_loss=torch.Tensor([8]).cuda() if use_gpu else torch.Tensor([8])
+    best_epoch_loss = torch.Tensor([8]).cuda() if use_gpu else torch.Tensor([8])
     for e in range(epoch):
         epoch_loss =torch.Tensor([0]).cuda() if use_gpu else torch.Tensor([0])
         epoch_eval_loss = torch.Tensor([0]).cuda() if use_gpu else torch.Tensor([0])
@@ -100,6 +101,11 @@ def t(load_path=None,fronzen=True,offset=0):
         epoch_part_loss =epoch_part_loss / len(test_loader)
         print("Train Epoch %d/%d| TrainMeanLoss: %.2f ,loc_loss : %.2f, conf_loss_obj: %.2f , conf_loss_no_obj: %.2f, cls_loss: %.2f " % (e + 1, epoch, epoch_loss/len(train_loader),epoch_part_loss[0],epoch_part_loss[1],epoch_part_loss[2],epoch_part_loss[3]))
 
+        if epoch_loss < best_epoch_loss and e > 10:
+            best_epoch_loss=epoch_loss
+            torch.save(model.state_dict(), './model/YOLOv1_softmax_sigmoid_Fronzen_best_train.pth')
+            print('best train model Saved')
+
         model.eval()
         with torch.no_grad():
             for i, (inputs, target,_) in enumerate(test_loader):
@@ -123,4 +129,4 @@ def t(load_path=None,fronzen=True,offset=0):
 
 if __name__ == '__main__':
     print('softmaxSigmoid fronzen=True,offset=0')
-    t(load_path='./model/YOLOv1_softmax_sigmoid_Fronzen_best.pth',fronzen=True,offset=0)
+    t(load_path='./model/YOLOv1_softmax_sigmoid_Fronzen_best.pth',fronzen=True,offset=80)
