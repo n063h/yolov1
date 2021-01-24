111e15bb827aa85a21aaeacfeffd89a19171be49
hang <20210240109@fudan.edu.cn>
2021-01-24 12:50:32 +0800

    bug fix

--- yolo/train.py
+++ yolo/train.py
@@ -127,5 +127,5 @@ def t(load_path=None,fronzen=True,offset=0):
 
 
 if __name__ == '__main__':
-    print('ceSigmoid fronzen=false,offset=0')
-    t(load_path='./model/YOLOv1.pth',fronzen=False,offset=0)
+    print('ceSigmoid fronzen=False,offset=80')
+    t(load_path='./model/YOLOv1.pth',fronzen=False,offset=80)
