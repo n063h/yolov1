75b1a8b47990b9e3e608e88f18f6b74682bc95cc
hang <20210240109@fudan.edu.cn>
2021-01-24 12:26:45 +0800

    bug fix

--- util/calc_map.py
+++ util/calc_map.py
@@ -132,7 +132,7 @@ def predict(model,img_path):
 use_gpu = torch.cuda.is_available()
 
 if __name__ == '__main__':
-    load_path='./model/YOLOv1_softmax_sigmoid_Fronzen_best.pth'
+    load_path='./model/YOLOv1.pth'
     model = vgg19_bn()
     if not use_gpu:
         model.load_state_dict(torch.load(load_path,map_location=torch.device('cpu')))
